a7717d69b6d07c2ae5958015ead769a6ae9f3718
messi.liao <krobor.lmx@gmail.com>
2017-09-26 16:13:59 +0800

    [feature] add exercise of "particle filters"

---
+++ problem/exercise_12_08.py
@@ -0,0 +1,69 @@
+# ----------
+# User Instructions:
+# 
+# Define a function, search() that returns a list
+# in the form of [optimal path length, row, col]. For
+# the grid shown below, your function should output
+# [11, 4, 5].
+#
+# If there is no valid path from the start point
+# to the goal, your function should return the string
+# 'fail'
+# ----------
+
+# Grid format:
+#   0 = Navigable space
+#   1 = Occupied space
+
+grid = [[0, 0, 1, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0],
+        [0, 0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 1, 0]]
+init = [0, 0]
+goal = [len(grid)-1, len(grid[0])-1]
+cost = 1
+
+delta = [[-1, 0], # go up
+         [ 0,-1], # go left
+         [ 1, 0], # go down
+         [ 0, 1]] # go right
+
+delta_name = ['^', '<', 'v', '>']
+
+def search(grid,init,goal,cost):
+    # ----------------------------------------
+    # insert code here
+    # ----------------------------------------
+    g_x, g_y = goal
+    try_path = [[0, init[0], init[1]]]
+    path = []
+    while True:
+        if len(try_path) == 0:
+            path = []
+            break
+
+        path = []
+        for c, x, y in try_path:
+            grid[x][y] = 1
+            if x == g_x and y == g_y:
+                path.append((c, x, y))
+        if len(path) > 0:
+            break
+        for c, x, y in try_path:
+            for dx, dy in delta:
+                nx, ny = x + dx, y + dy
+                valid_path = (c + cost, nx, ny)
+                if (0 <= nx < len(grid)) and (0 <= ny < len(grid[0])) and (grid[nx][ny] == 0) and valid_path not in path:
+                    path.append(valid_path)
+        try_path = path
+    try_path = []
+    for valid_path in path:
+        try_path.extend(valid_path)
+
+    path = try_path
+    if len(path) == 0:
+        path = 'fail'
+    return path
+
+print search(grid,init,goal,cost)
