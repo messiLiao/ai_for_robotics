318584dc0a6cce043cd26c472cdb17388d1a6ae4
figo_dev <figo_dev@jhhk.com>
2018-02-03 21:25:02 +0800

    add code for AdvancedLaneFinding

---
+++ problem/AdvancedLaneFinding_24.py
@@ -0,0 +1,86 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import pickle
+
+
+def abs_sobel_thresh(img, orient='x', sobel_kernel=3, thresh=(0, 255)):    
+    # Apply the following steps to img
+    # 1) Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # 2) Take the derivative in x or y given orient = 'x' or 'y'
+    if orient == 'x':
+        sobel = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    else:
+        sobel = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    # 3) Take the absolute value of the derivative or gradient
+    sobel_abs = np.absolute(sobel)
+    # 4) Scale to 8-bit (0 - 255) then convert to type = np.uint8
+    scaled_sobel = np.uint8(255*sobel_abs/np.max(sobel_abs))
+    # 5) Create a mask of 1's where the scaled gradient magnitude 
+            # is > thresh_min and < thresh_max
+    binary_output = np.zeros_like(scaled_sobel)
+    thresh_min, thresh_max = thresh
+    binary_output[(scaled_sobel >= thresh_min) & (scaled_sobel <= thresh_max)] = 1
+    # 6) Return this mask as your binary_output image
+    return binary_output
+
+def mag_thresh(image, sobel_kernel=3, mag_thresh=(0, 255)):    
+    # Apply the following steps to img
+    # 1) Convert to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    # 2) Take the gradient in x and y separately
+    sobel_x = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobel_y = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    # 3) Calculate the magnitude 
+    magnitude = np.sqrt(sobel_x * sobel_x + sobel_y * sobel_y)
+    # 4) Scale to 8-bit (0 - 255) and convert to type = np.uint8
+    scaled_sobel = np.uint8(255*magnitude/np.max(magnitude))
+    # 5) Create a binary mask where mag thresholds are met
+    binary_output = np.zeros_like(scaled_sobel)
+    thresh_min, thresh_max = mag_thresh
+    binary_output[(scaled_sobel >= thresh_min) & (scaled_sobel <= thresh_max)] = 1
+    # 6) Return this mask as your binary_output image
+    return binary_output
+
+def dir_threshold(image, sobel_kernel=3, thresh=(0, np.pi/2)):    
+    # Apply the following steps to img
+    # 1) Convert to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    # 2) Take the gradient in x and y separately
+    sobel_x = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel))
+    sobel_y = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel))
+    # 3) Take the absolute value of the x and y gradients
+    magnitude = np.sqrt(sobel_x * sobel_x + sobel_y * sobel_y)
+    # 4) Use np.arctan2(abs_sobely, abs_sobelx) to calculate the direction of the gradient 
+    grad_dir = np.arctan2(sobel_y, sobel_x)
+    # 5) Create a binary mask where direction thresholds are met
+    binary_output = np.zeros_like(grad_dir)
+    thresh_min, thresh_max = thresh
+    binary_output[(grad_dir >= thresh_min) & (grad_dir <= thresh_max)] = 1
+    # 6) Return this mask as your binary_output image
+    return binary_output
+
+# Read in an image
+image = mpimg.imread('signs_vehicles_xygrad.png')
+# Choose a Sobel kernel size
+ksize = 3 # Choose a larger odd number to smooth gradient measurements
+
+# Apply each of the thresholding functions
+gradx = abs_sobel_thresh(image, orient='x', sobel_kernel=ksize, thresh=(20, 100))
+grady = abs_sobel_thresh(image, orient='y', sobel_kernel=ksize, thresh=(20, 100))
+mag_binary = mag_thresh(image, sobel_kernel=ksize, mag_thresh=(30, 100))
+dir_binary = dir_threshold(image, sobel_kernel=ksize, thresh=(0.7, 1.2))
+combined = np.zeros_like(dir_binary)
+combined[((gradx == 1) & (grady == 1)) | ((mag_binary == 1) & (dir_binary == 1))] = 1
+
+# Plot the result
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+f.tight_layout()
+ax1.imshow(image)
+ax1.set_title('Original Image', fontsize=50)
+ax2.imshow(combined, cmap='gray')
+ax2.set_title('Thresholded Gradient', fontsize=50)
+plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+plt.show()
